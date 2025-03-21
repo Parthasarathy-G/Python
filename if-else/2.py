8b9f28817f3ced79c4be45cf23442e5e63af6a63
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-03-22 00:09:51 +0530

    while-loop file edited

---
+++ if-else/2.py
@@ -0,0 +1,19 @@
+x = int(input("Enter a number: ")) 
+i , y = 0, 0
+while x != i:
+    i+=1
+    y+=i
+
+print(y)
+
+# What is the output of this code?
+# 1. 1
+# 2. 3
+# 3. 6
+# 4. 10
+# 5. 15
+# 6. 21
+# 7. 28
+# 8. 36
+# 9. 45
+# 10. 55
